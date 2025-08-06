943b611133001ceb3f2cb36be8362f9be881f3c0
cabuelow <christinabuelow@gmail.com>
2025-08-06 12:45:50 +1000

    update new data and models

--- scripts/05_counterfactual-predictions.R
+++ scripts/05_counterfactual-predictions.R
@@ -5,20 +5,21 @@ library(brms)
 library(tidybayes)
 set.seed(123)
 
-load("outputs/models/global_models_zinb.rda")
-load("outputs/models/global_models_lognormal.rda")
-load("outputs/models/global_models_mult_outcome.rda")
-dat <- read.csv('data/fp_data_wrangled_2025-03-06.csv') |>
-  mutate(across(c(set_id:region_id, mpa_compliance, Shark_fishing_restrictions, Shark_Protection_Status, Shark_Sanctuary, mpa_present:Temporal_limits), factor),
+load("outputs/models/zinb_nomain_v2.rda")
+load("outputs/models/lognormal_nomain_v2.rda")
+load("outputs/models/binomial_nomain_v2.rda")
+dat <- read.csv('data/fp_data_wrangled_2025-08-05.csv') %>% 
+  mutate(set_composition = ifelse(is.na(set_composition), 'zero', set_composition),
+         across(c(set_id:Shark_Sanctuary, mpa_present, Area_limits:Temporal_limits, set_composition), factor),
          Shark_Protection_Status = relevel(factor(Shark_Protection_Status), ref = "Open"))
 
 # maxn ------------------------------
 
 # make predictions from the posterior and summarise outcomes at reef level
 # then get the median value for each reef and arrange from highest to lowest
-base_preds_zinb <- dat |> 
-  add_epred_draws(fit_zinb_int) |> # make predictions for each reef
-  group_by(reef_id, location_id, region_id) |> 
+base_preds_zinb <- dat %>% 
+  add_epred_draws(fit_zinb_int) %>% # make predictions for each reef
+  group_by(reef_id, location_id, region_id) %>% 
   summarise(Prediction_status_quo = median(.epred),
             upp_95_status_quo = quantile(.epred, 0.975),
             low_95_status_quo = quantile(.epred, 0.025),
@@ -27,11 +28,11 @@ base_preds_zinb <- dat |>
             upp_50_status_quo = quantile(.epred, 0.75),
             low_50_status_quo = quantile(.epred, 0.25))
 
-no_management_zinb <- dat |> 
+no_management_zinb <- dat %>% 
   # turn off management variables
-  mutate(Shark_Protection_Status = 'Open') |> 
-  add_epred_draws(fit_zinb_int) |> 
-  group_by(reef_id, location_id, region_id) |> 
+  mutate(Shark_Protection_Status = 'Open') %>% 
+  add_epred_draws(fit_zinb_int) %>% 
+  group_by(reef_id, location_id, region_id) %>% 
   summarise(Prediction_no_management = median(.epred),
             upp_95_no_management = quantile(.epred, 0.975),
             low_95_no_management = quantile(.epred, 0.025),
@@ -41,13 +42,13 @@ no_management_zinb <- dat |>
             low_50_no_management = quantile(.epred, 0.25))
 
 # calculate gains
-pred_zinb <- base_preds_zinb |>
-  left_join(no_management_zinb, by = c('reef_id', 'location_id', 'region_id')) |> 
+pred_zinb <- base_preds_zinb %>%
+  left_join(no_management_zinb, by = c('reef_id', 'location_id', 'region_id')) %>% 
   mutate(Gains = `Prediction_no_management`-`Prediction_status_quo`,
          upp_50 = `upp_50_no_management`-`upp_50_status_quo`,
-         low_50 = `low_50_no_management`-`low_50_status_quo`) |> 
-  ungroup() |> 
-  arrange(Gains) |> 
+         low_50 = `low_50_no_management`-`low_50_status_quo`) %>% 
+  ungroup() %>% 
+  arrange(Gains) %>% 
   mutate(Variable = 'Shark abundance',
          Scenario = 'No management',
          Site = 1:n(),
@@ -62,9 +63,9 @@ write.csv(pred_zinb, 'outputs/models/scenario-predictions_zinb.csv', row.names =
 
 # make predictions from the posterior and summarise outcomes at the reef level
 # then get the median value for each reef and arrange from highest to lowest
-base_preds_hu_lognormal <- dat |> 
-  add_epred_draws(fit_hu_lognormal_int) |> 
-  group_by(reef_id, location_id, region_id) |> 
+base_preds_hu_lognormal <- dat %>% 
+  add_epred_draws(fit_hu_lognormal_int) %>% 
+  group_by(reef_id, location_id, region_id) %>% 
   summarise(Prediction_status_quo = median(.epred),
             upp_95_status_quo = quantile(.epred, 0.975),
             low_95_status_quo = quantile(.epred, 0.025),
@@ -73,11 +74,11 @@ base_preds_hu_lognormal <- dat |>
             upp_50_status_quo = quantile(.epred, 0.75),
             low_50_status_quo = quantile(.epred, 0.25))
 
-no_management_hu_lognormal <- dat |> 
+no_management_hu_lognormal <- dat %>% 
   # turn off management variables
-  mutate(Shark_Protection_Status = 'Open') |> 
-  add_epred_draws(fit_hu_lognormal_int) |> 
-  group_by(reef_id, location_id, region_id) |> 
+  mutate(Shark_Protection_Status = 'Open') %>% 
+  add_epred_draws(fit_hu_lognormal_int) %>% 
+  group_by(reef_id, location_id, region_id) %>% 
   summarise(Prediction_no_management = median(.epred),
             upp_95_no_management = quantile(.epred, 0.975),
             low_95_no_management = quantile(.epred, 0.025),
@@ -87,13 +88,13 @@ no_management_hu_lognormal <- dat |>
             low_50_no_management = quantile(.epred, 0.25))
 
 # calculate gains
-pred_hu_lognormal <- base_preds_hu_lognormal |>
-  left_join(no_management_hu_lognormal, by = c('reef_id', 'location_id', 'region_id')) |> 
+pred_hu_lognormal <- base_preds_hu_lognormal %>%
+  left_join(no_management_hu_lognormal, by = c('reef_id', 'location_id', 'region_id')) %>% 
   mutate(Gains = `Prediction_no_management`-`Prediction_status_quo`,
          upp_50 = `upp_50_no_management`-`upp_50_status_quo`,
-         low_50 = `low_50_no_management`-`low_50_status_quo`) |> 
-  ungroup() |> 
-  arrange(Gains) |> 
+         low_50 = `low_50_no_management`-`low_50_status_quo`) %>% 
+  ungroup() %>% 
+  arrange(Gains) %>% 
   mutate(Variable = 'Predation potential',
          Scenario = 'No management',
          Site = 1:n(),
@@ -108,9 +109,9 @@ write.csv(pred_hu_lognormal, 'outputs/models/scenario-predictions_lognormal.csv'
 
 # make predictions from the posterior and summarise outcomes at the reef level
 # then get the median value for each reef and arrange from highest to lowest
-base_preds_prob_mult <- dat |> 
-  add_epred_draws(fit_prob_mult_int) |> 
-  group_by(reef_id, location_id, region_id) |> 
+base_preds_prob_mult <- dat %>% 
+  add_epred_draws(fit_prob_mult_int) %>% 
+  group_by(reef_id, location_id, region_id) %>% 
   summarise(Prediction_status_quo = median(.epred),
             upp_95_status_quo = quantile(.epred, 0.975),
             low_95_status_quo = quantile(.epred, 0.025),
@@ -119,11 +120,11 @@ base_preds_prob_mult <- dat |>
             upp_50_status_quo = quantile(.epred, 0.75),
             low_50_status_quo = quantile(.epred, 0.25))
 
-no_management_prob_mult <- dat |> 
+no_management_prob_mult <- dat %>% 
   # turn off management variables
-  mutate(Shark_Protection_Status = 'Open') |> 
-  add_epred_draws(fit_prob_mult_int) |> 
-  group_by(reef_id, location_id, region_id) |> 
+  mutate(Shark_Protection_Status = 'Open') %>% 
+  add_epred_draws(fit_prob_mult_int) %>% 
+  group_by(reef_id, location_id, region_id) %>% 
   summarise(Prediction_no_management = median(.epred),
             upp_95_no_management = quantile(.epred, 0.975),
             low_95_no_management = quantile(.epred, 0.025),
@@ -133,13 +134,13 @@ no_management_prob_mult <- dat |>
             low_50_no_management = quantile(.epred, 0.25))
 
 # calculate gains
-pred_prob_mult <- base_preds_prob_mult |>
-  left_join(no_management_prob_mult, by = c('reef_id', 'location_id', 'region_id')) |>  
+pred_prob_mult <- base_preds_prob_mult %>%
+  left_join(no_management_prob_mult, by = c('reef_id', 'location_id', 'region_id')) %>%  
   mutate(Gains = `Prediction_no_management`-`Prediction_status_quo`,
          upp_50 = `upp_50_no_management`-`upp_50_status_quo`,
-         low_50 = `low_50_no_management`-`low_50_status_quo`) |> 
-  ungroup() |> 
-  arrange(Gains) |> 
+         low_50 = `low_50_no_management`-`low_50_status_quo`) %>% 
+  ungroup() %>% 
+  arrange(Gains) %>% 
   mutate(Variable = 'Probability of co-benefits',
          Scenario = 'No management',
          Site = 1:n(),
